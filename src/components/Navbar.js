c5da4c9e9dda9a5fa09f5894c2bcbd05c8b406a1
alexfrenkel92 <alexfrenkel92@gmail.com>
2020-05-20 00:31:31 +0200

    react app created

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css';
+// import logo from '../img/market1.png';
+
+function Navbar() {
+    return (
+        <div className = 'navbar'>
+            {/* <img src={logo} class='logo' alt='market-place-logo' /> */}
+            Navbar to be
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Main from '../components/Main';
+
+function LandingPage() {
+    return (
+        <div className='landing-page'>
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
+
+export default LandingPage;
