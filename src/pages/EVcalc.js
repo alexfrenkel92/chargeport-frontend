165a5809e8e7a38cf4e24b98d3f89bb15fa87d4a
alexfrenkel92 <alexfrenkel92@gmail.com>
2020-06-08 16:15:43 +0200

    evCalc fixed

--- src/pages/EVcalc.js
+++ src/pages/EVcalc.js
@@ -35,14 +35,22 @@ function EVcalc () {
   const iceFuelCost100km = iceData.iceFuelPrice * iceData.iceConsumption;
   const iceFuelCostyear = mileageData.mileage * iceData.iceFuelPrice * iceData.iceConsumption / 100;
 
-  let saving = Math.round(iceFuelCostyear - evFuelCostyear);
-  if (Math.sign(saving) === -1) saving = 'A megtakarítás 0';
-
-  let timespan = 0;
-  if (evFuelCost100km > 0) {
-    timespan = Math.round((evData.evPrice - iceData.icePrice) / (iceFuelCostyear - evFuelCostyear));
+  function handleSaving() {
+    let saving = Math.round(iceFuelCostyear - evFuelCostyear);
+    if (Math.sign(saving) === -1) saving = 'A megtakarítás 0';
+    return saving;
   }
-  if (Math.sign(timespan) === -1) timespan = '0';
+
+  function handleTimespan() {
+    let timespan = 0;
+    if (!evData.evFuelPrice, evData.evConsumption, evData.evPrice) timespan = Math.round((evData.evPrice - iceData.icePrice) / (iceFuelCostyear));
+    if (mileageData.mileage, evData.evPrice, !evData.evConsumption || evData.evConsumption, !evData.evFuelPrice || evData.evFuelPrice) timespan = 0;
+    if (evFuelCost100km > 0) {
+      timespan = Math.round((evData.evPrice - iceData.icePrice) / (iceFuelCostyear - evFuelCostyear));
+    } 
+    if (Math.sign(timespan) === -1) timespan = '0';
+    return timespan;
+  }
 
   return (
     <div className='evcalc-wrapper'>
@@ -52,14 +60,10 @@ function EVcalc () {
         <p>Az időszakot lerövidítheti:</p>
         <li>otthoni napelemes rendszeről való töltés, amivel az üzemanyagköltség 0Ft/kW köré süllyedhet;</li>
         <li>a minnél nagyobb éves futásteljesítmény.</li>
-        <p />
-        <p />
-        <p />
-
       </div>
       <div className='evcalc-main'>
         <p className='titleEV'>Elektromos</p>
-        <p className='titleICE'>Belőégésű</p>
+        <p className='titleICE'>Belsőégésű</p>
         <p className='price evcalc'>Vételár</p>
         <p className='mileage evcalc'>Éves futásteljesítmény</p>
         <p className='fuel-cost evcalc'>Üzemanyagköltség</p>
@@ -69,28 +73,29 @@ function EVcalc () {
         <p className='difference evcalc'>Megtakarítás</p>
         <p className='duration evcalc'>Árparitás várható időtartalma</p>
 
+        <textarea className='mileage-merged' value={mileageData.mileage} onChange={handleMileage} placeholder='pl. 25.000km' />
+        <p className='mileage-merged-km'>km</p>
         <textarea className='ev-price' value={evData.evPrice} onChange={handleEVPrice} placeholder='pl. 8.000.000' />
         <p className='ev-price-Ft'>Ft</p>
         <textarea className='kW-price' value={evData.evFuelPrice} onChange={handleEVFuelPrice} placeholder='pl 40Ft' />
         <p className='kW-price-Ft'>Ft/kW</p>
         <textarea className='consumption-ev' value={evData.evConsumption} onChange={handleEVConsumption} placeholder='pl 16' />
         <p className='consumption ev-kw'>kW/100km</p>
-        <Button className='ev-fuel-cost-100km to-center' textContent={evFuelCost100km + ' Ft/100km'} />
-        <Button className='ev-fuel-cost-yearly to-center' textContent={evFuelCostyear + ' Ft/év'} />
-
         <textarea className='ice-price' value={iceData.icePrice} onChange={handleICEPrice} placeholder='pl. 5.000.000Ft' />
         <p className='ice-price-Ft'>Ft</p>
         <textarea className='l-price' value={iceData.iceFuelPrice} onChange={handleICEFuelPrice} placeholder='pl 380Ft' />
         <p className='l-price-Ft'>Ft/l</p>
         <textarea className='consumption-ice' value={iceData.iceConsumption} onChange={handleICEConsumption} placeholder='pl 7' />
         <p className='consumption ice-l'>l/100km</p>
+
+        <Button className='ev-fuel-cost-100km to-center' textContent={evFuelCost100km + ' Ft/100km'} />
+        <Button className='ev-fuel-cost-yearly to-center' textContent={evFuelCostyear + ' Ft/év'} />
+
         <Button className='ice-fuel-cost-100km to-center' textContent={iceFuelCost100km + ' Ft/100km'} />
         <Button className='ice-fuel-cost-yearly to-center' textContent={iceFuelCostyear + ' Ft/év'} />
 
-        <textarea className='mileage-merged' value={mileageData.mileage} onChange={handleMileage} placeholder='pl. 25.000km' />
-        <p className='mileage-merged-km'>km</p>
-        <Button className='saving to-center' textContent={saving + ' Ft/év'} />
-        <Button className='timespan to-center' textContent={timespan + ' év'} />
+        <Button className='saving to-center' textContent={handleSaving() + ' Ft/év'} />
+        <Button className='timespan to-center' textContent={handleTimespan() + ' év'} />
 
       </div>
     </div>
