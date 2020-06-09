faeb1fd5dd7bd92614d63df6d82fab20ecf5ee58
alexfrenkel92 <alexfrenkel92@gmail.com>
2020-06-09 18:14:09 +0200

    footer made smaller

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,6 @@ function Footer () {
       <div className='footer-content'>
         <p>Az oldal hobby projekt, a feltűntetett adatok nem valósak.</p>
         <p>alex.frenkel92@gmail.com</p>
-        <p>2020</p>
       </div>
     </div>
   );
