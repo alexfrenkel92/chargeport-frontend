da69e344090b88eb7c92b96b5f0e498c429c2fb2
alexfrenkel92 <alexfrenkel92@gmail.com>
2020-06-09 18:09:38 +0200

    aws configured

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -7,7 +7,7 @@ function aboutUs () {
       <p>Az oldal hobby projektként készült, gyakorlás céljából.</p>
       <p>A frontend React, backend node.js-l íródott.</p>
       <p>A hírlevélre történő feliratkozás során a név és e-mail cím nem kerül tárolásra, mindössze egy egyszeri e-mail kerül elküldésre.</p>
-      <p>A backend-ről származó DB adatok a https://www.freemysqlhosting.net/ oldalon vannak tárolva, aminek a rendelkezésre állása korlátozott, így előfordulhat, hogy néhány adat nem mindig tölt be.</p>
+      <p>Az adatok AWS adatbázisban vannak tárolva.</p>
     </div>
   );
 }
