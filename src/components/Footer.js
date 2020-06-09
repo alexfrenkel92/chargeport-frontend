f123117234b930bd289bd88c68414b70d7291f48
alexfrenkel92 <alexfrenkel92@gmail.com>
2020-06-09 12:38:30 +0200

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer () {
     <div className='footer'>
       <div className='footer-content'>
         <p>Az oldal hobby projekt, a feltűntetett adatok nem valósak.</p>
-        <p>Előfordulhat, hogy néhány a backend-ről származó adat nem tölt be, pl a Fórum és főoldalon.</p>
+        <p>Előfordulhat, hogy néhány a backend-ről származó adat nem tölt be, pl a főoladlon és Fórumon.</p>
         <p>alex.frenkel92@gmail.com</p>
       </div>
     </div>
