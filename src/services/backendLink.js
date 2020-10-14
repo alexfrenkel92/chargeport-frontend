b9d37a714ea295fa0c374647f09a425e7d9c0f87
alexfrenkel92 <alex.frenkel92@gmail.com>
2020-10-14 20:31:17 +0200

    url corrected

--- src/services/backendLink.js
+++ src/services/backendLink.js
@@ -3,7 +3,7 @@ let url = null;
 if (process.env.NODE_ENV === 'development') {
     url = `http://localhost:8080`
 } else {
-    url = 'https://chargeport-backend.herokuapp.com/'
+    url = 'https://chargeport-backend.herokuapp.com'
 }
 
 export default url
